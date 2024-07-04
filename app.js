aeeceec7936ebd9e8e7a11c6bb71153798d949c9
jossyAkin <josephayodejiakinyemi@gmail.com>
2024-07-04 18:15:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+
+//middleware
+app.use(cors())
+app.use(express.static('./public'));
+app.use(express.json());
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => console.log(` server is listening on port ${port}...`));
+        
+    }catch(error){
+        console.log(error)
+    }
+}
+
+start();
+
+
+
+
